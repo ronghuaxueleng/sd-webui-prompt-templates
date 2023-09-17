51191a8982e876c9e66f4849903b074018c8e737
caoqiang <caoqianghappy@126.com>
2023-09-17 16:27:33 +0800

    up

--- scripts/read_template.py
+++ scripts/read_template.py
@@ -19,11 +19,13 @@ def loadjsonfile(template_path):
             temp_list.append(template_dict['prompt'])
             temp_list.append(template_dict['NegativePrompt'])
             raw_encode = base64.b64encode(template_dict['raw'].encode("utf-8"))
+            print(raw_encode.decode('utf-8'))
+            onclick = 'prompt_send_to_txt2img("'+raw_encode.decode('utf-8')+'")'
             buttons = ("<div style='margin-top: 3px; text-align: center;'>"
                        "<button style='width: 102px;' class='secondary gradio-button svelte-cmf5ev'>详情</button>"
                        "</div>"
                        "<div style='margin-top: 3px; text-align: center;'>"
-                       "<button style='width: 102px;' class='secondary gradio-button svelte-cmf5ev' onclick='prompt_send_to_txt2img(\'"+raw_encode.decode('utf-8')+"\')'>发送到文生图</button>"
+                       "<button style='width: 102px;' class='secondary gradio-button svelte-cmf5ev' onclick='"+onclick+"'>发送到文生图</button>"
                        "</div>")
             temp_list.append(buttons)
             template_values.append(temp_list)
